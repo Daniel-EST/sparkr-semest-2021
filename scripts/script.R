e2623aca85a40bde12696ec4ce312ed77a24aa0c
Daniel-EST <dd_santos@id.uff.br>
2021-10-20 01:11:10 -0300

    FEAT: Adiciona comentários no script

--- scripts/script.R
+++ scripts/script.R
@@ -1,45 +1,59 @@
+################################################################################
+############################### SEMESTE 12 #####################################
+################################################################################
+######## Utilizando o R para Big Data, uma introdução prática ao SparkR ########
+################################################################################
+############################### 20/10/2021 #####################################
+################################################################################
+
+
+#### Inicialização
+# Checando instalação do Java
 system("java -version")
 
+# Definindo a localização do Spark no computador do usuário
 SPARK_HOME = "D:/spark-3.1.2-bin-hadoop2.7"
 
+# Definindo uma variável de ambiente SPARK_HOME
 Sys.setenv(SPARK_HOME=SPARK_HOME)
 
-
+# Carregando a biblioteca SparkR no caminho do Spark
 library(SparkR,
         lib.loc=c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 
-
+# Iniciando uma sessão do 
 sparkR.session(master = "local[*]",
                sparkConfig = list(spark.driver.memory = "1g"))
 
 sparkR.version()
 
+# Leitura dos dados
 dados = read.df(
   path = "../dados/okcupid_profiles.csv",
   source = "csv",
   delimiter = ",",
-  inferSchema = "true",
+  inferSchema = "true", # Inferir o tipo das colunas
   header = TRUE
 )
+
+### Conhecendo os dados
+printSchema(dados)
 
 nrow(dados)
 ncol(dados)
 
-printSchema(dados)
+View(head(dados, 5)) # Visualizar as 5 primeiras colunas
 
-View(head(dados, 5))
-
-# Retirando colunas
+# Retirando colunas não desejadas
 dados = drop(dados, "last_online")
 
 abertas = paste0("essay", 0:9)
 
 dados = drop(dados, abertas)
 
-# Visualizando...
 View(head(dados, 5))
 
-
+# Contagem de gênero
 gender_count = 
   collect(
     count(
@@ -48,7 +62,9 @@ gender_count =
     )
 print(gender_count)
 
-createOrReplaceTempView(dados, "okcupid")
+
+# Também podesse usar consultas SQL 
+createOrReplaceTempView(dados, "okcupid") # Cria uma tabela temporária para realização da query
 collect(
   sql(
     "
@@ -60,11 +76,11 @@ collect(
   )
 )
 
-
 require(ggplot2)
 ggplot(gender_count) +
   geom_col(aes(x = sex, y = count))
 
+# Separar a localização em cidade e estados
 dados = transform(dados, location_city = regexp_replace(dados$location, ", .*", ""))
 cidade_contagem = count(groupBy(dados, "location_city"))
 collect(arrange(cidade_contagem, -cidade_contagem$count))
@@ -73,19 +89,19 @@ dados = transform(dados, location_state = regexp_replace(dados$location, ".*, ",
 estado_contagem = count(groupBy(dados, "location_state"))
 collect(arrange(estado_contagem, -estado_contagem$count))
 
+# Retirar coluna que não tinha tratamento
 dados = drop(dados, dados$location)
 
 collect(
   agg(
-    groupBy(dados, dados$sex),
-      max_age = max(dados$age),
-      min_age = min(dados$age),
-      avg_age = avg(dados$age),
-      var_age = var(dados$age),
-      med_age = percentile_approx(dados$age, .5)
+    groupBy(dados, dados$sex), # Agrupando dados por gênero
+      max_age = max(dados$age), # Máximo
+      min_age = min(dados$age), # Mínimo
+      avg_age = avg(dados$age), # Média
+      var_age = var(dados$age), # Variância
+      med_age = percentile_approx(dados$age, .5) # Mediana
   )
 )
-
 
 status_count = 
   collect(
@@ -102,12 +118,15 @@ status_count$status = factor(status_count$status, unique(status_count$status))
 ggplot(status_count) +
   geom_col(aes(x = sex, y = count, fill = status), position = "dodge2")
 
+# Transformando em dados faltantes
 dados = withColumn(dados, "income", ifelse(dados$income == -1, NA, dados$income))
 
-NA_renda = filter(dados, isNull(dados$income))
+# Contagem de dados faltantes na renda
+NA_renda = filter(dados, isNull(dados$income)) # Filtrar dados faltantes na renda
 percent_NA_renda = nrow(NA_renda)/nrow(dados)
 print(percent_NA_renda)
 
+# Coletando informações para realização do boxplot
 boxplot_renda = collect(
   agg(
     groupBy(dados, dados$sex),
@@ -129,7 +148,7 @@ ggplot(boxplot_renda,
   ylab("Renda") + xlab("") +
   theme_minimal() 
 
-
+# Função para calcular histograma
 hist_data = histogram(dados, dados$age, nbins = 16)
 ggplot(hist_data) +
   geom_col(aes(x = centroids, y = counts))
@@ -159,3 +178,31 @@ ggplot(hist_data) +
 # ggplot(graph_data) +
 #   geom_col(aes(x = x_bin, y = count))
 
+
+caminho = "../dados/parquet"
+# Salvar dados particionados
+write.df(
+  dados, caminho, "parquet", mode = "overwrite",
+  partitionBy = "sex"
+)
+
+# Leitura dos dados particionados
+dados_part = read.df(
+  path = caminho,
+  source = "parquet",
+  delimiter = ",",
+  inferSchema = "true", # Inferir o tipo das colunas
+)
+
+View(head(dados_part))
+
+nrow(dados_part)
+ncol(dados_part)
+
+
+################################################################################
+########################## Obrigado por participar! ############################
+################################################################################
+###################### (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)  ########################
+################################################################################
+ 
